c5e4a605f8a40af36abd596eb1e38d3d499f2870
Damjan <damjan.dzalev1998@gmail.com>
2019-07-04 14:22:37 +0200

    finished level select and menu

--- AvengersTheFallen/AvengersTheFallen/Form1.Designer.cs
+++ AvengersTheFallen/AvengersTheFallen/Form1.Designer.cs
@@ -73,6 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
             this.ClientSize = new System.Drawing.Size(1312, 567);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
